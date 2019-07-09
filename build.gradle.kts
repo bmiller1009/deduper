917775b973e1b861988d5a3003bfe2bb49c5d5e0
Brad Miller <brad.miller@us.imshealth.com>
2019-07-09 11:04:10 -0400

    Configured packager

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,20 @@
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin on the JVM.
     id("org.jetbrains.kotlin.jvm").version("1.3.41")
-
+    id ("distribution")
     // Apply the application plugin to add support for building a CLI application.
     application
+}
+
+distributions {
+    getByName("main") {
+        baseName = "deduper"
+        contents {
+            from("src/main/resources") {
+                into("conf")
+            }
+        }
+    }
 }
 
 repositories {
