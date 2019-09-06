7cd778b830669e03d0575808819994f1b7d87704
Brad Miller <brad.miller@us.imshealth.com>
2019-09-06 16:37:53 -0400

    Added logic to dynamically add new jndi sources and append to existing jndi sources

--- src/main/kotlin/org/bradfordmiller/deduper/jndi/JNDIUtils.kt
+++ src/main/kotlin/org/bradfordmiller/deduper/jndi/JNDIUtils.kt
@@ -9,9 +9,8 @@ import java.io.*
 import java.rmi.activation.UnknownObjectException
 import java.sql.Connection
 import java.sql.SQLException
-import javax.naming.Context
-import javax.naming.InitialContext
-import javax.naming.NamingException
+import java.util.*
+import javax.naming.*
 import javax.sql.DataSource
 
 class JNDIUtils {
@@ -29,72 +28,100 @@ class JNDIUtils {
                 is Map<*, *> -> Right(lookup)
                 else -> {
                     throw UnknownObjectException(
-                        "jndi entry $jndi for context $context is neither an DataSource or a Map<String, String>"
+                            "jndi entry $jndi for context $context is neither an DataSource or a Map<String, String>"
                     )
                 }
             }
         }
+
         fun getConnection(ds: DataSource): Connection? {
             return try {
                 ds.connection
-            } catch(sqlEx: SQLException) {
+            } catch (sqlEx: SQLException) {
                 logger.error(sqlEx.message)
                 throw sqlEx
             }
         }
+
         fun getJndiConnection(jndiString: String, context: String): Connection {
             val jndi = (getDataSource(jndiString, context) as Left<DataSource?, String>).left!!
             return jndi.connection
         }
+
         //TODO: Make sure to put a lock file in place while updating the jndi root directory
-        fun addJndiConnection(jndiName: String, context: String, values: Map<String, String>) {
+        fun addJndiConnection(jndiName: String, context: String, values: Map<String, String>): Boolean {
+
+            fun writeToFile(backupFile: File, delimiter: String) {
+                BufferedWriter(FileWriter(backupFile.absolutePath, true)).use { bw ->
+                    bw.write("\n")
+                    values.forEach {
+                        val s = jndiName + delimiter + it.key + "=" + it.value
+                        bw.write("$s\n")
+                    }
+                }
+            }
 
             val ctx = InitialContext() as Context
 
             val root = ctx.environment.get("org.osjava.sj.root").toString()
             val colonReplace = ctx.environment.get("org.osjava.sj.colon.replace")
-            val delimiter = ctx.environment.get("org.osjava.sj.delimiter")
+            val delimiter = ctx.environment.get("org.osjava.sj.delimiter").toString()
+            val file = File("$root/$context.properties")
+            val lockFile = File("$root/.LOCK")
 
-            val memoryContext =
-                try {
-                    val mc = (ctx.lookup(context) as MemoryContext)
-                    mc
-                } catch(ne: NamingException) {
-                    logger.info("Naming exception occurred on jndi lookup of context $context: ${ne.message}")
-                    null
-                }
+            return try {
 
-            //context was found, so append to it
-            if(memoryContext != null) {
-                //Copy the context file
-                //Append the data to the copy
-                //Rename the copy to the original
-                //Delete the old file
+                if (lockFile.exists()) {
+                    logger.info("jndi director is currently being modified. Please try to add your new entry later.")
+                    false
+                } else {
+                    logger.info("Lock file is absent. Locking directory before proceeding")
+                    lockFile.createNewFile()
 
-
-            } else {
-
-                val file = File(root + "/$context.properties")
-                val fileCreated = file.createNewFile()
-                FileOutputStream(file).use { fos ->
-                    val lock = fos.channel.lock()
-                    try {
-                        val filePath = file.toRelativeString()
-                        BufferedWriter(FileWriter(filePath)).use { bw ->
-                            values.forEach {
-                                val s = jndiName + delimiter + it.key + "=" + it.value
-                                bw.write(s)
+                    val memoryContext =
+                            try {
+                                val mc = (ctx.lookup(context) as MemoryContext)
+                                mc
+                            } catch (ne: NamingException) {
+                                logger.info("Naming exception occurred on jndi lookup of context $context: ${ne.message}")
+                                null
                             }
+
+                    if (memoryContext != null) {
+                        //Check if the key being added already exists
+                        val field = memoryContext.javaClass.getDeclaredField("namesToObjects")
+                        field.isAccessible = true
+
+                        val fieldMap = field.get(memoryContext) as Map<Name, Object>
+                        val fieldKeys = fieldMap.keys.map { k -> k.toString() }
+
+                        if (fieldKeys.contains(jndiName)) {
+                            val errorString = "Jndi name $jndiName already exists for context $context."
+                            logger.error(errorString)
+                            throw IllegalAccessException(errorString)
+                        } else {
+                            val uuid = UUID.randomUUID().toString()
+                            val ap = file.absolutePath
+                            val backupFile = File(file.parentFile.name + "/" + context + "_" + uuid + ".properties")
+                            file.copyTo(backupFile, true)
+                            //Append the data to the copy
+                            writeToFile(backupFile, delimiter)
+                            //Rename the copy to the original
+                            backupFile.renameTo(file)
+                            true
                         }
-                    } finally {
-                        lock.release()
+
+                    } else {
+                        val f = File("$root/$context.properties")
+                        //Write the data to the copy
+                        writeToFile(f, delimiter)
+                        true
                     }
                 }
+            } finally {
+                logger.info("Deleting lock file. Directory is writable.")
+                lockFile.delete()
             }
-
-
-
-
         }
     }
 }
--- src/test/kotlin/JndiTest.kt
+++ src/test/kotlin/JndiTest.kt
@@ -1,50 +1,28 @@
-import org.apache.commons.dbcp2.BasicDataSource
-import org.bradfordmiller.deduper.Deduper
-import org.bradfordmiller.deduper.config.Config
 import org.bradfordmiller.deduper.jndi.JNDIUtils
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.BeforeAll
+
 import org.junit.jupiter.api.Test
-import org.osjava.sj.jndi.MemoryContext
-import org.slf4j.LoggerFactory
-import java.io.File
-import java.nio.file.Files
-import javax.naming.Context
-import javax.naming.InitialContext
 
 class JndiTest {
     @Test fun addNewJndiResource() {
 
         JNDIUtils.addJndiConnection(
             "BradTestJNDI",
-            "default_ds_2",
-            mapOf("targetName" to "src/test/resources/data/outputData/targetName")
+            "default_ds_3",
+            mapOf(
+                    "type" to "java.util.Map",
+                    "targetName" to "src/test/resources/data/outputData/ds3"
+                    )
         )
+    }
 
-        /*val ds = BasicDataSource()
-
-        ds.driverClassName = "org.sqlite.JDBC"
-        ds.url = "jdbc:sqlite:src/test/resources/data/outputData/real_estate.db"
-
-        val ctx = InitialContext() as Context
-
-        //ctx.addToEnvironment("BradTestJNDI", ds)
-
-        //ctx.close()
-        //ctx.environment.
-
-
-
-        val mc = (ctx.lookup("default_ds") as MemoryContext)
-
-        //val list = mc.
-
-        //mc.addToEnvironment("SqlLiteTesting", ds)
-
-        val root = mc.environment.get("org.osjava.sj.root")
-
-        mc.bind("SqlLiteTesting", ds)
-
-        mc.close()*/
+    @Test fun addNewEntrytoExistingJndi() {
+        JNDIUtils.addJndiConnection(
+                "BradTestJNDI_22",
+                "default_ds",
+                mapOf(
+                        "type" to "java.util.Map",
+                        "targetName" to "src/test/resources/data/outputData/testJNDI"
+                )
+        )
     }
 }
