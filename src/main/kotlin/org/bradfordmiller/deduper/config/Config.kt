f58bdbdb1ff441e035aa5ffba8fa86678f06bd95
Brad Miller <bfm@bradfordmiller.org>
2019-09-23 23:17:54 -0400

    Started adding support for deleting pre-existing target/dupes

--- src/main/kotlin/org/bradfordmiller/deduper/config/Config.kt
+++ src/main/kotlin/org/bradfordmiller/deduper/config/Config.kt
@@ -12,7 +12,9 @@ class Config private constructor(
     val hashColumns: Set<String>,
     val tgtJndi: String?,
     val dupesJndi: String?,
-    val tgtTable: String?
+    val tgtTable: String?,
+    val deleteTargetIfExists: Boolean,
+    val deleteDupeIfExist: Boolean
 ) {
 
     companion object {
@@ -26,7 +28,9 @@ class Config private constructor(
         private var keyOn: Set<String>? = null,
         private var tgtJndi: String? = null,
         private var dupesJndi: String? = null,
-        private var tgtTable: String? = null
+        private var tgtTable: String? = null,
+        private var deleteTargetIfExist: Boolean = false,
+        private var deleteDupeIfExist: Boolean = false
     ) {
         companion object {
             private val logger = LoggerFactory.getLogger(ConfigBuilder::class.java)
@@ -39,13 +43,17 @@ class Config private constructor(
         fun targetJndi(tgtJndi: String) = apply { this.tgtJndi = tgtJndi }
         fun dupesJndi(dupesJndi: String) = apply { this.dupesJndi = dupesJndi }
         fun targetTable(targetTable: String) = apply { this.tgtTable = targetTable }
+        fun deleteTargetIfExists(deleteTargetIfExist: Boolean) = apply {this.deleteTargetIfExist = deleteTargetIfExist}
+        fun deleteDupeIfExists(deleteDupeIfExist: Boolean) = apply {this.deleteDupeIfExist = deleteDupeIfExist}
         fun build(): Config {
             val sourceJndi = srcJndi ?: throw NullArgumentException("Source JNDI must be set")
             val sourceName = srcName ?: throw NullArgumentException("Source JNDI name must be set")
             val finalContext = context ?: throw NullArgumentException("JNDI context must be set")
             val targetJndi = tgtJndi
             val finalDupesJndi = dupesJndi
-            val config = Config(sourceJndi, sourceName, finalContext, keyOn.orEmpty(), targetJndi, finalDupesJndi, tgtTable)
+            val finalDeleteTargetIfExists = deleteTargetIfExist
+            val finalDeleteDupeIfExists = deleteDupeIfExist
+            val config = Config(sourceJndi, sourceName, finalContext, keyOn.orEmpty(), targetJndi, finalDupesJndi, tgtTable, finalDeleteTargetIfExists, finalDeleteDupeIfExists)
             logger.trace("Built config object $config")
             return config
         }
