1969e98f04aa67af4ca6dadc50b7df2fa299e93c
Brad Miller <bfm@bradfordmiller.org>
2019-10-20 21:47:58 -0400

    Kdoc'd CsvConfigParser class

--- src/main/kotlin/org/bradfordmiller/deduper/csv/CsvConfigParser.kt
+++ src/main/kotlin/org/bradfordmiller/deduper/csv/CsvConfigParser.kt
@@ -4,7 +4,11 @@ import org.bradfordmiller.deduper.jndi.JNDIUtils
 import org.bradfordmiller.deduper.utils.Right
 import org.slf4j.LoggerFactory
 
-class CsvConfigParser(config: Map<String, String>) {
+/**
+ * Parses [config] settings which define the format for outputting csv data.  Note that the default file extension for
+ * a csv output is 'txt' and the default delimiter is a comma
+ */
+class CsvConfigParser(private val config: Map<String, String>) {
 
     val extension = setDefault(config, "ext", "txt")
     val delimiter = setDefault(config, "delimiter", ",")
@@ -13,12 +17,16 @@ class CsvConfigParser(config: Map<String, String>) {
     companion object {
 
         val logger = LoggerFactory.getLogger(CsvConfigParser::class.java)
-
+        /**
+         * returns a configuration map of csv formatting values based on the lookup of the [jndi] in the jndi [context]
+         */
         fun getCsvMap(context: String, jndi: String): Map<String, String> {
             val ds = JNDIUtils.getDataSource(jndi, context) as Right
             return ds.right as Map<String, String>
         }
-
+        /**
+         * sets default values in the csv configuration map if parameters are not provided
+         */
         fun setDefault(config: Map<String, String>, key: String, default: String): String {
             if(!config.containsKey(key) || config[key] == "") {
                 logger.info("'$key' key is blank.  Using '$default' as extension for default")
